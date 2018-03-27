8fb518501a06195eed4bf84c9f311bf393748751
Miguel Angel <miguelangel140598@gmail.com>
2018-03-27 09:37:30 -0600

    Cambio en Clases

--- app/src/main/java/com/example/miguelangel/savenergy/Menu_configuracion.java
+++ app/src/main/java/com/example/miguelangel/savenergy/Menu_configuracion.java
@@ -7,21 +7,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link Menu_configuracion.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link Menu_configuracion#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class Menu_configuracion extends Fragment implements Configuracion_Perfil.OnFragmentInteractionListener {
+public class Menu_configuracion extends Fragment implements View.OnClickListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    Button btn_configurar_perfil;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -33,8 +28,6 @@ public class Menu_configuracion extends Fragment implements Configuracion_Perfil
         // Required empty public constructor
     }
 
-
-    // TODO: Rename and change types and number of parameters
     public static Menu_configuracion newInstance(String param1, String param2) {
         Menu_configuracion fragment = new Menu_configuracion();
         Bundle args = new Bundle();
@@ -51,6 +44,9 @@ public class Menu_configuracion extends Fragment implements Configuracion_Perfil
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+        btn_configurar_perfil = (Button) btn_configurar_perfil.findViewById(R.id.iniciar_sesion);
+        btn_configurar_perfil.setOnClickListener(this);
     }
 
     @Override
@@ -58,9 +54,10 @@ public class Menu_configuracion extends Fragment implements Configuracion_Perfil
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_menu_configuracion, container, false);
+
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
+
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
@@ -84,21 +81,25 @@ public class Menu_configuracion extends Fragment implements Configuracion_Perfil
         mListener = null;
     }
 
+
     @Override
-    public void onFragmentInteraction(Uri uri) {
+    public void onClick(View view) {
+        if(view == btn_configurar_perfil){
+            configurar_perfil();
+
+        }
+    }
+
+    Fragment fragment = null;
+
+    public void configurar_perfil() {
+
+        fragment = new Configuracion_Perfil();
+        
+
 
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
