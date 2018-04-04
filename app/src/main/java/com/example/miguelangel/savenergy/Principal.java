2c9146664d3a0f20f7f47c461829bbbaf03c6051
Miguel Angel <miguelangel140598@gmail.com>
2018-04-04 13:56:03 -0500

    Grafica Terminada

--- app/src/main/java/com/example/miguelangel/savenergy/Principal.java
+++ app/src/main/java/com/example/miguelangel/savenergy/Principal.java
@@ -3,6 +3,7 @@ package com.example.miguelangel.savenergy;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.service.autofill.Dataset;
 import android.support.v4.app.Fragment;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,14 +15,26 @@ import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.Chart;
 import com.github.mikephil.charting.charts.CombinedChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.LegendEntry;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.DataSet;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+
+import java.util.ArrayList;
 
 public class Principal extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, Configuracion_Perfil.OnFragmentInteractionListener,
 Bienvenido.OnFragmentInteractionListener, Menu_configuracion.OnFragmentInteractionListener{
 
-    private CombinedChart grafica;
+    private BarChart grafica;
     private String []horas = new String[]{"1","2","3","4","5","6","7","8"};
     private int[]volts = new int[]{100,200,300,400,500,600,700,800,900,1000,1100,1200,1300,1400,1500,1600,};
     private int[]colores = new int[]{Color.rgb(13,13,13)};
@@ -52,7 +65,8 @@ Bienvenido.OnFragmentInteractionListener, Menu_configuracion.OnFragmentInteracti
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        grafica = (CombinedChart) findViewById(R.id.grafica);
+        grafica = (BarChart) findViewById(R.id.grafica);
+        createCharts();
     }
 
     @Override
@@ -134,7 +148,72 @@ Bienvenido.OnFragmentInteractionListener, Menu_configuracion.OnFragmentInteracti
 
     }
 
-    private Chart getSameChart(){
-        return null;
+    private Chart getSameChart(Chart chart, String description, int textColor, int background, int animation){
+        chart.getDescription().setText(description);
+        chart.getDescription().setTextSize(15);
+        chart.setBackgroundColor(background);
+        chart.animateY(animation);
+        legend(chart);
+        return chart;
+    }
+
+    private void legend(Chart chart){
+        Legend legend = chart.getLegend();
+        legend.setForm(Legend.LegendForm.CIRCLE);
+        legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
+
+        ArrayList<LegendEntry>entries = new ArrayList<>();
+        for (int i=0; i<horas.length; i++){
+            LegendEntry entry = new LegendEntry();
+            entry.label=horas[i];
+            entries.add(entry);
+        }
+        legend.setCustom(entries);
+    }
+
+    private ArrayList<BarEntry>getBarEntries(){
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        for (int i=0; i<volts.length; i++) {
+            entries.add(new BarEntry(i,volts[i]));
+        }
+        return entries;
+    }
+
+    private void axisX(XAxis axis){
+        axis.setGranularityEnabled(true);
+        axis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        axis.setValueFormatter(new IndexAxisValueFormatter(horas));
+    }
+
+    private void axisLeft(YAxis axis){
+        axis.setSpaceTop(1000);
+        axis.setAxisMinimum(0);
+    }
+
+    private void axisRight(YAxis axis){
+        axis.setEnabled(false);
+    }
+
+    public void createCharts(){
+        grafica = (BarChart)getSameChart(grafica,"Consumo de Energías",Color.WHITE,Color.BLACK,3000);
+        grafica.setData(getBarData());
+        grafica.invalidate();
+        axisX(grafica.getXAxis());
+        axisLeft(grafica.getAxisLeft());
+        axisRight(grafica.getAxisRight());
+    }
+
+    private DataSet getData(DataSet dataSet){
+        dataSet.setColors(colores);
+        dataSet.setValueTextSize(Color.WHITE);
+        dataSet.setValueTextSize(10);
+        return dataSet;
+    }
+
+    private BarData getBarData(){
+        BarDataSet barDataSet = (BarDataSet) getData(new BarDataSet(getBarEntries(),""));
+        BarData barData = new BarData(barDataSet);
+        barData.setBarWidth(1);
+        return barData;
     }
 }
