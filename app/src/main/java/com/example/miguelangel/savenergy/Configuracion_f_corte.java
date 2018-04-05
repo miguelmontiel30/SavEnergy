81907b1396b8f5fb1ba443934f91d044e3942c03
pablocm07 <1716110094@utec-tgo.edu.mx>
2018-04-05 13:56:53 -0500

    Activity Configuracion_f_corte, creando Spinner

--- app/src/main/java/com/example/miguelangel/savenergy/Configuracion_f_corte.java
+++ app/src/main/java/com/example/miguelangel/savenergy/Configuracion_f_corte.java
@@ -2,12 +2,43 @@ package com.example.miguelangel.savenergy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Spinner;
+
+import com.loopj.android.http.*;
+
+import cz.msebera.android.httpclient.Header;
 
 public class Configuracion_f_corte extends AppCompatActivity {
+
+    private AsyncHttpClient cliente;
+    private Spinner spTarifas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuracion_f_corte);
+
+        cliente = new AsyncHttpClient();
+        spTarifas = (Spinner) findViewById(R.id.sp_tarifa);
+    }
+
+    private void llenarSp(){
+        String url = "https://savenergy.000webhostapp.com/savenergy/get_Tarifas.php";
+        cliente.post(url, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                if(statusCode==200){
+                    cargarSpinner(new String(responseBody));
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+            }
+        });
+    }
+    private void cargarSpinner(String respuesta){
+
     }
 }
